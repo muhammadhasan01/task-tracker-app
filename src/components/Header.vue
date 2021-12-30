1f8bf0d8f6f6aa1ce6e224438189cd0fa7abbe09
Muhammad Hasan <39514247+muhammadhasan01@users.noreply.github.com>
2021-12-31 03:05:44 +0700

    feat: add initial Header

---
+++ src/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+  <header>
+    <h1>Task Tracker</h1>
+  </header>
+</template>
+
+<script lang="ts">
+export default {
+  name: "Header"
+}
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+</style>
